66886d945c92cc5dceb6f97e81d2ebce382a012c
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 02:09:38 +0300

    [.] - Added admin.py for User

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('-last_login',)
+    readonly_fields = ('last_login',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Dates', {'fields': ('last_login',)}),
+    )
