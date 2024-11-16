c6c4666d94c53b5d123de20e850c6ee2b99ed09c
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 22:50:08 +0300

    [.] - Update TimeStampedModel

--- src/core/models.py
+++ src/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from typing import Optional, List
 
 
 class TimeStampedModel(models.Model):
@@ -10,14 +11,25 @@ class TimeStampedModel(models.Model):
         abstract = True
 
     def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None, # noqa
+        self, force_insert: bool = False, force_update: bool = False, using: Optional[str] = None,
+        update_fields: Optional[List[str]] = None
     ) -> None:
-        # https://docs.djangoproject.com/en/5.1/ref/models/fields/#django.db.models.DateField.auto_now
+        """
+        Overrides the save method to update the updated_at field on each save.
+
+        Updates the 'updated_at' field to the current time before saving the object to the database.
+
+        :param force_insert: If True, the object will be inserted as new (default is False).
+        :param force_update: If True, the object will be updated (defaults to False).
+        :param using: The database that will be used for saving (default None).
+        :param update_fields: A list or set of fields to be updated.
+        """
         self.updated_at = timezone.now()
 
-        if isinstance(update_fields, list):
-            update_fields.append('updated_at')
-        elif isinstance(update_fields, set):
-            update_fields.add('updated_at')
+        if update_fields is not None:
+            if isinstance(update_fields, list):
+                update_fields.append('updated_at')
+            elif isinstance(update_fields, set):
+                update_fields.add('updated_at')
 
         super().save(force_insert, force_update, using, update_fields)
