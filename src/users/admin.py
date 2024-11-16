c08b7d3f93e5a437dae46604d51cc30de58de8fd
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:38:49 +0300

    [:mag:] - Enhance list_filter with created/modified fields

--- src/users/admin.py
+++ src/users/admin.py
@@ -7,7 +7,7 @@ class UserAdmin(admin.ModelAdmin):
     """Admin panel for User model with optimized display settings."""
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
-    list_filter = ('is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff', 'created', 'modified')
     ordering = ('-last_login',)
     readonly_fields = ('last_login',)
 
