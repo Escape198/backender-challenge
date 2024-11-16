7bdd5a8ee799924ff423535e76eb86f36992290a
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:28:53 +0300

    [.] - Correction LogClient

--- src/core/event_log_client.py
+++ src/core/event_log_client.py
@@ -51,27 +51,24 @@ class EventLogClient:
         finally:
             client.close()
 
-    def insert(self, data: List[Model]) -> None:
-        """Inserting data into ClickHouse."""
+    def insert(self, data: List[Model], batch_size: int = 100) -> None:
+        """
+        Inserts events into ClickHouse with batching support.
+
+        :param data: List of events to insert.
+        :param batch_size: Batch size for batch insertion (default is 100).
+        """
         try:
-            converted_data = self._convert_data(data)
-            self._client.insert(
-                data=converted_data,
-                column_names=EVENT_LOG_COLUMNS,
-                database=self._schema,
-                table=self._table,
-            )
-            logger.info(
-                "Successfully inserted data into ClickHouse",
-                row_count=len(converted_data),
-            )
+            for i in range(0, len(data), batch_size):
+                batch = data[i:i + batch_size]
+                self._client.insert(
+                    data=self._convert_data(batch), column_names=EVENT_LOG_COLUMNS, database=self._schema,
+                    table=self._table, )
+                logger.info(
+                    "Batch inserted into ClickHouse", batch_size=len(batch), total_records=len(data),
+                    batch_number=(i // batch_size) + 1, )
         except DatabaseError as e:
-            logger.error(
-                "Unable to insert data into ClickHouse",
-                error=str(e),
-                table=self._table,
-                schema=self._schema,
-            )
+            logger.error("Failed to insert data into ClickHouse", error=str(e))
             raise
 
     def query(self, query: str) -> List[tuple[Any]]:
