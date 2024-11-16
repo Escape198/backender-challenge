0cf57bca9048c05c54a48b6a9b76da2ab6a3a783
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:42:17 +0300

    [fix] - Translate admin panel fieldset labels to English

--- src/users/admin.py
+++ src/users/admin.py
@@ -7,7 +7,7 @@ class UserAdmin(admin.ModelAdmin):
     """Admin panel for User model with optimized display settings."""
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
-    list_filter = ('is_active', 'is_staff', 'created', 'modified')
+    list_filter = ('is_active', 'is_staff')
     ordering = ('-last_login',)
     readonly_fields = ('last_login', 'password')
 
