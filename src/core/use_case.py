956ebf5077ddd23ee2839f05466d63c8722c0c0b
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:47:33 +0300

    [refactor] - UseCase class refactored to use ABC for abstract methods

--- src/core/use_case.py
+++ src/core/use_case.py
@@ -1,4 +1,5 @@
-from typing import Any, Protocol
+from typing import Any, Protocol, Dict
+from abc import ABC, abstractmethod
 
 import structlog
 from django.db import transaction
@@ -20,32 +21,44 @@ class UseCaseResponse(Model):
     error: str = ''
 
 
-class UseCase(Protocol):
-    """A protocol that defines the interface for all Use Cases."""
+class UseCase(ABC):
+    """Abstract class defining the interface for all Use Cases."""
+
     def execute(self, request: UseCaseRequest) -> UseCaseResponse:
-        """The primary method of execution is Use Case."""
+        """
+        The primary method of execution for the Use Case.
+
+        Subclasses may override this method for custom behavior.
+        """
         with structlog.contextvars.bound_contextvars(**self._get_context_vars(request)):
             logger.info("Executing use case", use_case=self.__class__.__name__)
             return self._execute(request)
 
-    def _get_context_vars(self, request: UseCaseRequest) -> dict[str, Any]:  # noqa: ARG002
+    def _get_context_vars(self, request: UseCaseRequest) -> Dict[str, Any]:
         """
         Generates context variables for logging.
-        !!! WARNING:
-            Do not query the database in this method.
-        """
-        return {"use_case": self.__class__.__name__,}
 
+        WARNING:
+        Do not query the database in this method.
+        """
+        return {"use_case": self.__class__.__name__}
+
+    @abstractmethod
     def _execute(self, request: UseCaseRequest) -> UseCaseResponse:
         """An abstract method that must be implemented in subclasses."""
-        raise NotImplementedError("Subclasses must implement '_execute'")
+        pass
 
 
 class TransactionalUseCase(UseCase):
     """Abstract Use Case with transactional execution."""
+
     @transaction.atomic
     def execute(self, request: UseCaseRequest) -> UseCaseResponse:
-        """Executing a Use Case in a Transaction."""
+        """
+        Execute the Use Case within a database transaction.
+
+        Overrides the base execute method to wrap the operation in an atomic block.
+        """
         with structlog.contextvars.bound_contextvars(**self._get_context_vars(request)):
             logger.info("Executing transactional use case", use_case=self.__class__.__name__)
             return self._execute(request)
