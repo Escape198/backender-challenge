7a5b64bb1b2e6f8508478de474a1e12d2c4acebd
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 22:38:25 +0300

    [.] - Update use_case.py

--- src/core/use_case.py
+++ src/core/use_case.py
@@ -6,32 +6,46 @@ from django.db import transaction
 from core.base_model import Model
 
 
+logger = structlog.get_logger(__name__)
+
+
 class UseCaseRequest(Model):
+    """The base class for all queries."""
     pass
 
 
 class UseCaseResponse(Model):
+    """A basic class for all answers."""
     result: Any = None
     error: str = ''
 
 
 class UseCase(Protocol):
+    """A protocol that defines the interface for all Use Cases."""
     def execute(self, request: UseCaseRequest) -> UseCaseResponse:
-        with structlog.contextvars.bound_contextvars(
-            **self._get_context_vars(request),
-        ):
+        """The primary method of execution is Use Case."""
+        with structlog.contextvars.bound_contextvars(**self._get_context_vars(request)):
+            logger.info("Executing use case", use_case=self.__class__.__name__)
             return self._execute(request)
 
     def _get_context_vars(self, request: UseCaseRequest) -> dict[str, Any]:  # noqa: ARG002
         """
+        Generates context variables for logging.
         !!! WARNING:
-            This method is calling out of transaction so do not make db
-            queries in this method.
+            Do not query the database in this method.
         """
-        return {
-            'use_case': self.__class__.__name__,
-        }
+        return {"use_case": self.__class__.__name__,}
 
-    @transaction.atomic()
     def _execute(self, request: UseCaseRequest) -> UseCaseResponse:
-        raise NotImplementedError()
+        """An abstract method that must be implemented in subclasses."""
+        raise NotImplementedError("Subclasses must implement '_execute'")
+
+
+class TransactionalUseCase(UseCase):
+    """Abstract Use Case with transactional execution."""
+    @transaction.atomic
+    def execute(self, request: UseCaseRequest) -> UseCaseResponse:
+        """Executing a Use Case in a Transaction."""
+        with structlog.contextvars.bound_contextvars(**self._get_context_vars(request)):
+            logger.info("Executing transactional use case", use_case=self.__class__.__name__)
+            return self._execute(request)
