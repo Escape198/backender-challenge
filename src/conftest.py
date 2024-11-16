a678129753af7f04055a3f4535850a4f4a334292
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:51:31 +0300

    [fix] - Temp

--- src/conftest.py
+++ src/conftest.py
@@ -1,4 +1,3 @@
-import os
 import clickhouse_connect
 import pytest
 from clickhouse_connect.driver import Client
@@ -6,10 +5,6 @@ from clickhouse_connect.driver import Client
 
 @pytest.fixture(scope='module')
 def f_ch_client() -> Client:
-    """Fixture for providing a ClickHouse client."""
-    clickhouse_host = os.getenv('CLICKHOUSE_HOST', 'localhost')
-    clickhouse_port = os.getenv('CLICKHOUSE_PORT', '9000')
-
-    client = clickhouse_connect.get_client(host=clickhouse_host, port=int(clickhouse_port))
+    client = clickhouse_connect.get_client(host='clickhouse')
     yield client
     client.close()
