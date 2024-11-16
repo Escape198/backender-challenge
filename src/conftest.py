6de56a4d51e622fbf1aefa372850738c40b33b01
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 23:44:31 +0300

    [fix] - Use environment variables for ClickHouse client configuration in tests

--- src/conftest.py
+++ src/conftest.py
@@ -1,3 +1,4 @@
+import os
 import clickhouse_connect
 import pytest
 from clickhouse_connect.driver import Client
@@ -5,6 +6,10 @@ from clickhouse_connect.driver import Client
 
 @pytest.fixture(scope='module')
 def f_ch_client() -> Client:
-    client = clickhouse_connect.get_client(host='clickhouse')
+    """Fixture for providing a ClickHouse client."""
+    clickhouse_host = os.getenv('CLICKHOUSE_HOST', 'localhost')
+    clickhouse_port = os.getenv('CLICKHOUSE_PORT', '9000')
+
+    client = clickhouse_connect.get_client(host=clickhouse_host, port=int(clickhouse_port))
     yield client
     client.close()
