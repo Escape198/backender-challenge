e47b9c9322bac577e4c47793d665febd296d8ee2
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-16 22:23:36 +0300

    [.] - Add user's admin.py

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    """
+    Админ-панель для модели User с оптимизированными настройками отображения.
+    """
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('-date_joined',)
+    readonly_fields = ('last_login', 'date_joined')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личная информация', {'fields': ('first_name', 'last_name')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
