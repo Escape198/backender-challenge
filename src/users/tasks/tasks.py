af7cd028ff3e91b2e09062f17b79b53529aead30
Ruslan Kirzhanov <ruslan.kirzhanov@fashionfactoryschool.ru>
2024-11-18 12:17:16 +0300

    [fix] - A. The task for publishing events is reliable

--- src/users/tasks/tasks.py
+++ src/users/tasks/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
-from core.log_service import log_user_creation_event
 from django.db import transaction
+
+from core.log_service import log_user_creation_event
+from core.event_log_client import EventLogClient
 import structlog
 
 logger = structlog.get_logger(__name__)
@@ -9,18 +11,14 @@ logger = structlog.get_logger(__name__)
 @shared_task(max_retries=3, default_retry_delay=10)
 def log_user_creation(user_id, event_data):
     """
-    Task to log user creation events in ClickHouse.
-
-    Args:
-        user_id: ID of the user being logged.
-        event_data: Dictionary containing event information.
+    Task to log user creation events in ClickHouse with transactional and retry safety.
     """
     try:
-        logger.info("Attempting to log user creation event", user_id=user_id)
         with transaction.atomic():
-            log_user_creation_event(user_id=user_id, event_data=event_data)
-        logger.info("User creation event logged successfully", user_id=user_id)
-
+            logger.info("Attempting to log event in ClickHouse", user_id=user_id)
+            with EventLogClient.init() as client:
+                log_user_creation_event(user_id, event_data)
+            logger.info("User creation event logged successfully", user_id=user_id)
     except Exception as e:
         logger.error("Failed to log user creation event", user_id=user_id, error=str(e))
         raise log_user_creation.retry(exc=e)
